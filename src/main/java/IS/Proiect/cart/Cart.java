f7ddf43721146473c9485e291ce6134531754999
Claudiu <piticujoc@yahoo.com>
2024-12-04 22:43:41 +0200

    CosDoarFrontEnd

---
+++ src/main/java/IS/Proiect/cart/Cart.java
@@ -0,0 +1,66 @@
+package IS.Proiect.cart;
+
+import IS.Proiect.car.Car;
+import jakarta.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table
+public class Cart {
+    @Id
+    @SequenceGenerator(
+            name = "cart_sequence",
+            sequenceName = "cart_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "cart_sequence"
+    )
+    private Integer idCart;
+
+    @OneToMany
+    @JoinColumn(name = "cart_id", referencedColumnName = "idCart")
+    private List<Car> cars = new ArrayList<>();
+
+    public Cart() {
+    }
+
+    public Cart(Integer idCart, List<Car> cars) {
+        this.idCart = idCart;
+        this.cars = cars;
+    }
+
+    public Integer getIdCart() {
+        return idCart;
+    }
+
+    public void setIdCart(Integer idCart) {
+        this.idCart = idCart;
+    }
+
+    public List<Car> getCars() {
+        return cars;
+    }
+
+    public void setCars(List<Car> cars) {
+        this.cars = cars;
+    }
+
+    public void addCar(Car car) {
+        this.cars.add(car);
+    }
+
+    public void removeCar(Car car) {
+        this.cars.remove(car);
+    }
+
+    @Override
+    public String toString() {
+        return "Cart{" +
+                "idCart=" + idCart +
+                ", cars=" + cars +
+                '}';
+    }
+}
---
+++ src/main/java/IS/Proiect/cart/CartService.java
@@ -0,0 +1,41 @@
+package IS.Proiect.cart;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+@Service
+public class CartService {
+    @Autowired
+    private final CartRepository cartRepository;
+
+    public CartService(CartRepository cartRepository) {
+        this.cartRepository = cartRepository;
+    }
+
+    public List<Cart> getCarts() {
+        return cartRepository.findAll();
+    }
+
+    public Cart addNewCart(Cart cart) {
+        cartRepository.save(cart);
+        return cart;
+    }
+
+    public void deleteCart(Integer id) {
+        boolean exists = cartRepository.existsById(id);
+        if (!exists) {
+            throw new IllegalStateException("Cart with id " + id + " doesn't exists");
+        }
+        cartRepository.deleteById(id);
+    }
+
+    public void updateCart(Integer id, Cart updatedcart) {
+        Cart cart = cartRepository.findById(id).orElseThrow(() -> new IllegalStateException("cart with id " + id + " doesn't exist"));
+        cartRepository.save(cart);
+    }
+}
